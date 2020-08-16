fa396b7dc469eadbb5f31f76553fcc22535fa199
ivosdc (via Travis CI) <ivo@oss.volkswagen.de>
2020-08-16 21:43:06 +0000

    Deploy ivosdc/svelte-labeled-input to github.com/ivosdc/svelte-labeled-input.git:gh-pages

--- build/labeled-input.mjs
+++ build/labeled-input.mjs
@@ -368,7 +368,7 @@ var LabeledInput = (function () {
     	return [value, name, placeholder, label, input_input_handler];
     }
 
-    class LabeledInput$1 extends SvelteElement {
+    class LabeledInput extends SvelteElement {
     	constructor(options) {
     		super();
     		this.shadowRoot.innerHTML = `<style>.field{display:flex;flex-flow:column-reverse;margin-bottom:1.5em}label,input{transition:all 0.2s;touch-action:manipulation}label{position:relative;font-size:0.8em;top:0em}input{font-size:1em;border:0;border-bottom:1px solid #ccc;font-family:inherit;font-size:1em;font-weight:200;-webkit-appearance:none;border-radius:0;padding:0;cursor:text}input:focus{outline:0;border-bottom:1px solid #666}input:placeholder-shown+label{cursor:text;max-width:0%;white-space:nowrap;overflow:unset;text-overflow:ellipsis;transform-origin:left top;transform:translate(0, 1.2em) scale(1.2)}::-webkit-input-placeholder{opacity:0;transition:inherit}::-moz-placeholder{opacity:0;transition:inherit}input:focus::-webkit-input-placeholder{opacity:0.5}input:focus::-moz-placeholder{opacity:0.5}input:not(:placeholder-shown)+label,input:focus+label{transform:translate(0, 0) scale(1);cursor:pointer}</style>`;
@@ -433,7 +433,7 @@ var LabeledInput = (function () {
     	}
     }
 
-    customElements.define("labeled-input", LabeledInput$1);
+    customElements.define("labeled-input", LabeledInput);
 
     return LabeledInput;
 
